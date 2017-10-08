5d72376da4a16ed4d0573a3cfded6dedb79f7fa0
Alisha Kewalramani <alikew24@gmail.com>
2017-10-08 12:42:42 -0400

    Add instructions and make final edits

--- towers_of_hanoi.rb
+++ towers_of_hanoi.rb
@@ -1,4 +1,6 @@
 require 'byebug'
+require 'io/console'
+
 class TowersOfHanoi
   attr_accessor :towers
 
@@ -6,22 +8,54 @@ class TowersOfHanoi
     @towers = [[5, 3, 1], [], []]
   end
 
+  def show_instructions
+    system("clear")
+    puts <<-HEREDOC
+    In this game, the goal is to move all the disks from Tower 1
+    to one of the other towers. You must adhere to the following rules:
+      1. You can only move one disk at a time.
+      2. A larger disk cannot be placed on top of a smaller disk.
+
+    The disks are depicted as follows:
+      Smallest disk: ♟
+      Medium-sized disk: ♟♟♟
+      Largest disk: ♟♟♟♟♟
+    HEREDOC
+
+    puts "\nPress any key to continue!"
+    STDIN.getch
+    system("clear")
+
+    puts <<-HEREDOC
+    To play, first select which tower you would like to move the disk
+    from by typing '1', '2', or '3', and then pressing enter.
+
+    Then, select which tower you would like to move that disk to.
+    HEREDOC
+
+    puts "\nPress any key to begin playing!"
+    STDIN.getch
+    system("clear")
+  end
+
   def run
+    self.show_instructions
     while (!self.has_won?)
-      system("clear")
       display
-      puts "What block would you like to move from?"
+      puts "What tower would you like to move from?"
       from = gets.chomp.to_i - 1
-      puts "What block would you like to move to?"
+      puts "What tower would you like to move to?"
       to = gets.chomp.to_i - 1
       if valid_move?(from, to)
         move(from, to)
-        display
+        system("clear")
       else
+        system("clear")
         puts "That is not a valid move"
       end
     end
-    puts "congrats! You won the game!"
+    display
+    puts "Congrats! You won the game!"
   end
 
   def move(from, to)
@@ -31,6 +65,7 @@ class TowersOfHanoi
 
   def valid_move?(from, to)
     return false if [from, to].any? { |num| num > 2 || num < 0 }
+    return false if from == to
     return false if @towers[from].empty?
     return true if @towers[to].empty?
 
